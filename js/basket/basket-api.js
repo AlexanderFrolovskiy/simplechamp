1cbec66d02a1c27ba69431bf211fe6a3b46ba711
aleksandr_frolovskii <a.v.frolovskii@gmail.com>
2022-12-03 01:13:14 +0300

    add form

--- js/basket/basket-api.js
+++
@@ -1,8 +0,0 @@
-export function countPlus() {
-    const plus = document.querySelector('.basket__tbody-count__plus');
-    let parent = plus.closest('.basket__tbody-count');
-
-    console.log(parent);
-
-
-}
